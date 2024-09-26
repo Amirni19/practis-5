a0be508575a457e3b66c446770c2dbcab128adce
Amirni19 <160733644+Amirni19@users.noreply.github.com>
2024-09-26 17:15:10 +0300

    main.js

--- java/main.js
+++ java/main.js
@@ -1,67 +1,65 @@
 'use strict';
 
-// let salaries = {
-//     John: 100,
-//     Ann: 160,
-//     Pete: 130
-//   }
-//   let sum = arr.salaries((sum, item) => sum + item, 0);
-// alert(sum);
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  };
+  let sum = 0;
+  for (let key in salaries) {
+    sum += salaries[key];
+  }
+  alert(sum);
 
-// let menu = {
-//   width: 200,
-//   height: 300,
-//   title: "My menu"
-// };
-// multiplyNumeric(menu);
-// menu = {
-//   width: 400,
-//   height: 600,
-//   title: "My menu"
-// };
-// function multiplyNumeric(object) {
-//      for (let key in object) {
-//       if (typeof obj[key] == 'number') {
-//       obj[key] *= 2;
-//     }
-//   }
-// }
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+ function multiplyNumeric(obj) {
+    for(key in obj) {
+       if (typeof obj[key] === 'number') {
+          obj[key] *= 2;
+       }
+    }
+    return obj;
+ }
+  console.log( multiplyNumeric(menu) );
 
-// let calculator = {
-//     read() {
-//         this.a = +prompt('a?', 0);
-//         this.b = +prompt('b?', 0);
-//       },
-//     sum() {
-//         return this.a + this.b;
-//       },
-//     mul() {
-//         return this.a * this.b;
-//       }
-// };
-// calculator.read();
-// alert( calculator.sum());
-// alert( calculator.mul());
+let calculator = {
+    read() {
+        this.a = +prompt('a?', 0);
+        this.b = +prompt('b?', 0);
+      },
+    sum() {
+        return this.a + this.b;
+      },
+    mul() {
+        return this.a * this.b;
+      }
+};
+calculator.read();
+alert( calculator.sum());
+alert( calculator.mul());
 
-// let users = ['$120']
-// let user = users.slice(1);
-// alert(user);
+function extractCurrencyValue(str) {
+    return +str.slice(1);
+  }
+  alert( extractCurrencyValue('$120'));
 
-// let vasya = { name: "Вася", age: 25 };
-// let petya = { name: "Петя", age: 30 };
-// let masha = { name: "Маша", age: 28 };
+  
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+let users = [ vasya, petya, masha ];
+let names = users.map(item => item.name);
+alert( names );
 
-// let users = [ vasya, petya, masha ];
-
-// let names = users.map(item => item.name);
-
-// alert( names );
-
-// function getAverageAge(users) {
-//     return users.reduce((prev, user) => prev + user.age, 0) / users.length;
-// }
-// let age = {age: 27}
-// let age1 = {age: 67}
-// let age2 = {age: 36}
-// let ages = [age, age1, age2];
-// alert(getAverageAge(ages));
+function getAverageAge(users) {
+    return users.reduce((prev, user) => prev + user.age, 0) / users.length;
+}
+let age = {age: 27}
+let age1 = {age: 67}
+let age2 = {age: 36}
+let ages = [age, age1, age2];
+alert(getAverageAge(ages));
